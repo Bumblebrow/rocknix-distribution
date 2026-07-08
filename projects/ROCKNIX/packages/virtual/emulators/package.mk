d58e042d10b6985af4e49eac943c6dce09f4a4e7
Bumblebrow <wesley.fielder@gmail.com>
2026-07-08 17:55:52 -0500

    pcsx2-sa: removed due to stble yaps2-sa

--- projects/ROCKNIX/packages/virtual/emulators/package.mk
+++ projects/ROCKNIX/packages/virtual/emulators/package.mk
@@ -61,13 +61,13 @@ case "${DEVICE}" in
   SM8250)
     [ "${ENABLE_32BIT}" == "true" ] && EMUS_32BIT="box86 daedalusx64-sa desmume-lr gpsp-lr pcsx_rearmed-lr"
     PKG_EMUS+=" aethersx2-sa azahar-sa bigpemu-sa cemu-sa dolphin-sa heroic mednafen melonds-sa nanoboyadvance-sa rpcs3-sa supermodel-sa \
-                xemu-sa skyemu-sa steam vita3k-sa yaps2-sa pcsx2-sa citron-sa eden-sa scummvmsa"
+                xemu-sa skyemu-sa steam vita3k-sa yaps2-sa citron-sa eden-sa"
     LIBRETRO_CORES+=" beetle-psx-lr beetle-saturn-lr bsnes-lr bsnes-hd-lr dolphin-lr kronos-lr"
     ;;
   SM8550)
     [ "${ENABLE_32BIT}" == "true" ] && EMUS_32BIT="box86 daedalusx64-sa desmume-lr gpsp-lr pcsx_rearmed-lr"
     PKG_EMUS+=" aethersx2-sa ares-sa azahar-sa bigpemu-sa cemu-sa dolphin-sa drastic-sa gopher64-sa heroic mednafen melonds-sa nanoboyadvance-sa rpcs3-sa supermodel-sa \
-                xemu-sa skyemu-sa steam vita3k-sa yaps2-sa"
+                xemu-sa skyemu-sa steam vita3k-sa yaps2-sa citron-sa eden-sa"
     LIBRETRO_CORES+=" beetle-psx-lr beetle-saturn-lr bsnes-lr bsnes-hd-lr dolphin-lr kronos-lr"
     ;;
   SM8650|SM8750)
