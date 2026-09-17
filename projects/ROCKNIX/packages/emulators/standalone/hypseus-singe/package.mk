549f48fff05a31681af7ad06f23d114b6dc1f469
Bumblebrow <wesley.fielder@gmail.com>
2026-09-16 22:21:29 -0500

    hypseus-singe: rolled back

--- projects/ROCKNIX/packages/emulators/standalone/hypseus-singe/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/hypseus-singe/package.mk
@@ -13,17 +13,20 @@ PKG_TOOLCHAIN="cmake"
 
 PKG_CMAKE_OPTS_TARGET=" ./src"
 
-post_makeinstall_target() {
-  mkdir -p ${INSTALL}/usr/bin
-    cp -a ${PKG_DIR}/scripts/start_hypseus.sh ${INSTALL}/usr/bin
+pre_patch() {
+  cp -a ${PKG_DIR}/scripts/start_hypseus.sh ${PKG_BUILD}/
+}
 
+pre_configure_target() {
   mkdir -p ${INSTALL}/usr/config/game/configs/hypseus
-    cp -a ${PKG_BUILD}/doc/hypinput.ini ${INSTALL}/usr/config/game/configs/hypseus
-    ln -fs /storage/roms/daphne/roms ${INSTALL}/usr/config/game/configs/hypseus/roms
-    ln -fs /usr/share/daphne/sound ${INSTALL}/usr/config/game/configs/hypseus/sound
-    ln -fs /usr/share/daphne/fonts ${INSTALL}/usr/config/game/configs/hypseus/fonts
-    ln -fs /usr/share/daphne/pics ${INSTALL}/usr/config/game/configs/hypseus/pics
+  ln -fs /storage/roms/daphne/roms ${INSTALL}/usr/config/game/configs/hypseus/roms
+  ln -fs /usr/share/daphne/sound ${INSTALL}/usr/config/game/configs/hypseus/sound
+  ln -fs /usr/share/daphne/fonts ${INSTALL}/usr/config/game/configs/hypseus/fonts
+  ln -fs /usr/share/daphne/pics ${INSTALL}/usr/config/game/configs/hypseus/pics
+}
 
-  mkdir -p ${INSTALL}/usr/share/daphne
-    ln -fs /storage/.config/game/configs/hypseus/hypinput.ini ${INSTALL}/usr/share/daphne/hypinput.ini
+post_makeinstall_target() {
+  cp -rf ${PKG_BUILD}/doc/hypinput.ini ${INSTALL}/usr/config/game/configs/hypseus/
+  ln -fs /storage/.config/game/configs/hypseus/hypinput.ini ${INSTALL}/usr/share/daphne/hypinput.ini
+  cp ${PKG_BUILD}/start_hypseus.sh ${INSTALL}/usr/bin
 }
